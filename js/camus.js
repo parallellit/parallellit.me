a52b6255b44af912ee48ccc16efe8971f25e051e
parallellit <parallellit@gmail.com>
2014-11-20 16:57:43 -0800

    edited modulo

--- js/camus.js
+++ js/camus.js
@@ -56,7 +56,7 @@ function readTranslation(filename, index) {
 	   		$(full_translation_id).html(snt[index]);
 	   		$('.end-sentence').each(function(j) {
 			    // Store an id with format "uniqueId_{index}" in a variable.
-			    var my_class = "end-sentence_" + (j % 151); // assumes 1:1 matching for all
+			    var my_class = "end-sentence_" + (j % 306); // assumes 1:1 matching for all
 			    var id = index + "_" + my_class; 
 			    // Give the ID to the div
 			    $(this).attr("class", my_class);
