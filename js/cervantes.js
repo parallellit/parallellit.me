20cf2e5b4c756ff4e93bc03df3527b863f0239af
parallellit <parallellit@gmail.com>
2014-12-04 14:57:05 -0800

    Update cervantes.js

--- js/cervantes.js
+++ js/cervantes.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 	    $('#end-pane').scrollTop($(this).scrollTop());
 	});
 	
-	$('#main-text').append('<div class="instruction" style="border: 1px solid;border-radius: 3px; background:#111; color:white; padding: 5px; height: 40px;">To get started, pick a main text to examine:</div>')
+	$('#main-text').append('<div class="instruction" style="border-radius: 3px; background:#111; color:white; vertical-align: middle; padding: 5px; height: 40px;">To get started, pick a main text to examine:</div>')
 });
 
 function readTranslation(filename, index) {
