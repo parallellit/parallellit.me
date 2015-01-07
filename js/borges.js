25ab5b5b5c52efc13afc3bc1e5b4a4a0fd2ce53b
parallellit <parallellit@gmail.com>
2015-01-06 19:28:21 -0800

    Update borges.js

--- js/borges.js
+++ js/borges.js
@@ -5,77 +5,59 @@ $(document).ready(function() {
 	// Check for the various File API support.
 	if (window.File && window.FileReader && window.FileList && window.Blob) {
 		console.log('Hooray! The File APIs are fully supported by this browser!');
-
 		for (i = 0; i < NUM_FILES; i++) {
 			filename = './texts/borges/' + i + '.txt';
-			// if (i == 0) {
-			// 	readOriginal(filename);
-			// } else {
-				readTranslation(filename, i);
-			// }
+			readTranslation(filename, i);
 		}
 	} else {
 		alert('The File APIs are not fully supported by your browser.');
 	}
+	
+	$('#main-text').on('scroll', function () {
+	    $('#end-pane').scrollTop($(this).scrollTop());
+	});
+	
+	$('#main-text').append('<div class="instruction" style="border: 1px solid; border-color: #33CCFF; height: 22px; border-radius: 3px; background:#111; opacity: 0.8; color:#33CCFF; padding: 5px; ">To get started, pick a main text to examine:</div>')
 });
 
-// function displayButton() {
-
-// }
-
-// function readOriginal(filename) {
-// 	jQuery.get(filename, function(data) {
-// 		snt[0] = data.replace(/([^.!?]*[^.!?\s][.!?]['"]?)(\s|$)/g, 
-// 	   	'<span class="sentence">$1</span>$2');
-
-// 		displayButtons(); 
-// 		// $('#main-text').html(snt[0]);
-// 		// note that here this will only affect the main-text section
-// 		$('.sentence').each(function(i) {
-// 		    // Store an id with format "uniqueId_{index}" in a variable.
-// 		    var id = "0_sentence_" + i;
-// 		    // Give the ID to the div
-// 		    $(this).attr("id", id);
-// 		});
-// 	});
-// }
-
 function readTranslation(filename, index) {
-	// console.log(filename);
+	// setTimeout(loadPreview(index), 0);
 	jQuery.get(filename, function(data) {
+		console.log('readTranslation(' + index + ')');
+		
 		snt.push(data.replace(/([^.!?]*[^.!?\s][.!?]['"]?)(\s|$)/g, 
 	   	'<span class="end-sentence">$1</span>$2'));
-	   	// console.log(index);
-
-	   	// end pane itself is not visible at this point
-	   	// console.log(snt);
+	   	// empty divs
 	   	var full_translation = '<div class=\"full-translation\" id=\"full-translation_' + index +'\" style=\"width:100%;border-radius:3px;\"></div>';
-	   	var full_translation_id = '#full-translation_' + index;
 	   	$('#end-pane').append(full_translation);
-	   	$('.full-translation').each(function(i) {
-	   		$(full_translation_id).html(snt[index]);
-	   		$('.end-sentence').each(function(j) {
-			    // Store an id with format "uniqueId_{index}" in a variable.
-			    var my_class = "end-sentence_" + j; // assumes 1:1 matching for all
-			    var id = index + "_" + my_class; 
-			    // Give the ID to the div
-			    $(this).attr("class", my_class);
-			    $(this).attr("id", id);
-			});
-			// snt[index] = $('.full-translation').html(); <-- never implemented but might be useful
-
-	   		var line = "";
-	   		$('.end-sentence_0').each(function(k) {
-			   	var end_sentence_id = '#' + k + '_end-sentence_0';
-			   	console.log('END: ' + end_sentence_id);
-			   	console.log('val: ' + $(end_sentence_id).html());
-			   	line += '<div class=\"main-text_preview\" id=\"main-text_preview_' + (k) + '\" style=\"width:100%;padding:5px;padding-right:20px;border-radius:3px;\">' + $(end_sentence_id).html() + '</div>';
-			});
-			$('#main-text').html(line);
-	   	});
-
-	   	// var line = "";
+	   	// console.log('#end-pane: ' + $('#end-pane').html());
+	}).done(function(e) {
+		console.log('FILL full_translation(' + index + ')');
+		var full_translation_id = '#full-translation_' + index;
+		$(full_translation_id).html(snt[index]);
+	}).done(function(e) {
+		console.log('update/d end-sentence (' + index + ')');
+		
+		$('.end-sentence').each(function(j) {
+			if (j == 0) {
+				console.log('ZERO!');
+			}
+			if (j == 1) {
+				var end_sentence_id = '#' + index + '_end-sentence_0';
+				var line = '<div class=\"main-text_preview\" id=\"main-text_preview_' + (index) + '\" style=\"width:100%;padding:5px;padding-right:20px;border-radius:3px;\">' + $(end_sentence_id).text() + '</div>';
+				console.log('endT: ' + $(end_sentence_id).text());
+				$('#main-text').append(line);
+			}
+			var my_class = "end-sentence_" + j; // assumes 1:1 matching for all, so a class looks like end-sentence_0
+			var id = index + "_" + my_class; 
+			// Give the ID to the div
+			$(this).attr("class", my_class);
+			$(this).attr("id", id);
+			console.log('id: ' + id);
+		});
 	});
+	// .done(function(e) {
+	// });
 }
 
 $(document).on('click','.main-text_preview', function() {
@@ -88,16 +70,14 @@ $(document).on('click','.main-text_preview', function() {
 		var id = main_preview_number + "_" + my_class + "_" + i;
 		$(this).attr("class", my_class);
 		$(this).attr("id", id);
-		console.log('MY ID: ' + $(this).attr("id"));
+		// console.log('MY ID: ' + $(this).attr("id"));
 	});
-	// var preview_number = event.target.id.slice(-1);
-	// var translation_to_view_id = '#full-translation_' + preview_number; 
-	// $('.full-translation').css("display", "none");
-	// // console.log(translation_to_view_id);
-	// $(translation_to_view_id).css("display","block");
-	// $('#end-pane').css("display","block");
-	// $('.preview').css("background","none");
-	// $(preview_id).css("background","#F1F1F1");
+});
+
+$(document).on('mouseover', '.sentence', function() {
+	$('.sentence').css("background", "transparent"); // clear previously highlighted sentences
+	$(this).css("background", "#F1F1F1"); // highlight the clicked text
+	console.log('hover!');
 });
 
 $(document).on('click','.sentence', function() {
